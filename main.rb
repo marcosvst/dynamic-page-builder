61cdea57cc7c7421fdd781311a2d6da6afe1ceb0
Marcos Vinícius <marcosvst98@gmail.com>
2020-12-07 18:11:00 -0300

    feat: add nav header and 404 page

--- main.rb
+++ main.rb
@@ -9,11 +9,9 @@ get '/:endpoint' do
     file_content = File.read("contents/#{file_name}")
     file_components = YAML.safe_load(file_content)
 
-    @page_content = file_components
-
-    erb :default_template
+    erb :default_template, :locals => file_components
   rescue StandardError => e
     puts "Failed to load the endpoint because: #{e}"
-    "Oops! Seems like this page doesn't exist"
+    erb :not_found_template
   end
 end
